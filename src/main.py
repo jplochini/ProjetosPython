f6a157605ce01a3718591148430fa340b934a854
Joao Pedro Lochini <jp.lochini@gmail.com>
2024-03-13 02:47:06 -0300

    criando o pre processamento de imagens

---
+++ src/main.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import pandas as pd
+from skimage import io
+from sklearn.model_selection import train_test_split
+import cv2
+
+def pre_processing_images(dataset):
+    imagens_pre_processadas = []
+
+    for image_path in os.listdir(dataset):
+        image = cv2.imread(os.path.join(dataset, image_path))
+
+        #redimensionar a imagem para tamanho fixo
+        image = cv2.resize(image, (150, 150))
+
+        #converter a imagem para escala de cinza
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        #normalizar a imagem
+        image = cv2.normalize(image, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+        imagens_pre_processadas.append(image)
+
+    return imagens_pre_processadas
+
+diretorio = 'datasets/archive/apenas_testando'
+imagens = pre_processing_images(diretorio)
+
+#visualizar imagens
+import matplotlib.pyplot as plt
+for i in range(5):
+    plt.subplot(1, 5, i+1)
+    plt.imshow(imagens[i], cmap='gray')
+    plt.axis('off')
+
+plt.show()
